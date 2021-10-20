c04d14c243b8e293baebe7698bbeca26d84ee989
mujicapm <41750235+mujicapm@users.noreply.github.com>
2021-10-20 17:43:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useReducer } from "react";
 import UserBar from "./User/UserBar";
 import CreateToDoItem from "./ToDos/CreateToDoItem";
 import ToDoList from "./ToDos/ToDoList";
-import handleDateCreated from "./HandleDate";
+import appReducer from './Reducers';
+// import handleDateCreated from "./HandleDate";
 
 function App() {
   const initialToDoItems = [
@@ -32,77 +33,79 @@ function App() {
     },
   ];
 
-  function userReducer(state, action) {
-    switch (action.type) {
-      case "LOGIN":
-      case "REGISTER":
-        return action.username;
-      case "LOGOUT":
-        return "";
-      default:
-        return state;
-    }
-  }
+  const [ state, dispatch ] = useReducer(appReducer, { user: '', ToDoItems: initialToDoItems })
+  const {user, ToDoItems} = state;
+  // function userReducer(state, action) {
+  //   switch (action.type) {
+  //     case "LOGIN":
+  //     case "REGISTER":
+  //       return action.username;
+  //     case "LOGOUT":
+  //       return "";
+  //     default:
+  //       return state;
+  //   }
+  // }
+  //
+  // function toDoReducer(state, action) {
+  //   switch (action.type) {
+  //     case "CREATE_TODO":
+  //       const newPost = {
+  //         UUID: action.UUID,
+  //         title: action.title,
+  //         description: action.description,
+  //         dateCreated: action.dateCreated,
+  //         isComplete: false,
+  //         dateComplete: "",
+  //       };
+  //       return [newPost, ...state];
+  //     case "TOGGLE_TODO":
+  //       if (action.isComplete) {
+  //         return state.map((toDoItem) =>
+  //             toDoItem.UUID === action.UUID
+  //                 ? {
+  //                   ...toDoItem,
+  //                   isComplete: !action.isComplete,
+  //                   dateComplete: "",
+  //                 }
+  //                 : toDoItem
+  //         );
+  //       } else {
+  //         return state.map((toDoItem) =>
+  //             toDoItem.UUID === action.UUID
+  //                 ? {
+  //                   ...toDoItem,
+  //                   isComplete: !action.isComplete,
+  //                   dateComplete: handleDateCreated(),
+  //                 }
+  //                 : toDoItem
+  //         );
+  //       }
+  //     case "DELETE_TODO":
+  //       const newList = state.filter(
+  //           (toDoItem) => toDoItem.UUID !== action.UUID
+  //       );
+  //       return newList;
+  //     default:
+  //       return state;
+  //   }
+  // }
 
-  function toDoReducer(state, action) {
-    switch (action.type) {
-      case "CREATE_TODO":
-        const newPost = {
-          UUID: action.UUID,
-          title: action.title,
-          description: action.description,
-          dateCreated: action.dateCreated,
-          isComplete: false,
-          dateComplete: "",
-        };
-        return [newPost, ...state];
-      case "TOGGLE_TODO":
-        if (action.isComplete) {
-          return state.map((toDoItem) =>
-            toDoItem.UUID === action.UUID
-              ? {
-                  ...toDoItem,
-                  isComplete: !action.isComplete,
-                  dateComplete: "",
-                }
-              : toDoItem
-          );
-        } else {
-          return state.map((toDoItem) =>
-            toDoItem.UUID === action.UUID
-              ? {
-                  ...toDoItem,
-                  isComplete: !action.isComplete,
-                  dateComplete: handleDateCreated(),
-                }
-              : toDoItem
-          );
-        }
-      case "DELETE_TODO":
-        const newList = state.filter(
-          (toDoItem) => toDoItem.UUID !== action.UUID
-        );
-        return newList;
-      default:
-        return state;
-    }
-  }
-
-  const [user, dispatchUser] = useReducer(userReducer, "");
-  const [ToDoItems, dispatchToDo] = useReducer(toDoReducer, initialToDoItems);
+  // const [user, dispatchUser] = useReducer(userReducer, "");
+  // const [ToDoItems, dispatchToDo] = useReducer(toDoReducer, initialToDoItems);
 
   return (
-    <div>
-      <UserBar user={user} dispatchUser={dispatchUser} />
-      <br />
-      <br />
-      <hr />
-      <br />
-      {user && (
-        <CreateToDoItem ToDoItems={ToDoItems} dispatchToDo={dispatchToDo} />
-      )}
-      <ToDoList ToDoItems={ToDoItems} dispatchToDo={dispatchToDo} />
-    </div>
+      <div>
+        <UserBar user={user} dispatch={dispatch} />
+        <br />
+        <br />
+        <hr />
+        <br />
+        {user && (
+            <CreateToDoItem ToDoItems={ToDoItems} dispatch={dispatch} />
+        )}
+        <ToDoList ToDoItems={ToDoItems} dispatch={dispatch} />
+      </div>
   );
 }
 
